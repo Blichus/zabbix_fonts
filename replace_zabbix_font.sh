c3efffe4d2a10e8072cc823a2b5a67a3cf033322
Junnan <yingjunnan1996@gmail.com>
2022-04-11 15:17:48 +0800

    Update replace_zabbix_font.sh
    添加重启zabbix-server步骤

--- replace_zabbix_font.sh
+++ replace_zabbix_font.sh
@@ -6,3 +6,4 @@ echo "备份原软链接"
 mv /etc/alternatives/zabbix-web-font /etc/alternatives/zabbix-web-font-bak
 echo "新建软链接"
 ln -s /usr/share/fonts/dejavu/msyh.ttf /etc/alternatives/zabbix-web-font
+systemctl restart zabbix-server
