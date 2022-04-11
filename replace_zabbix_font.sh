39928b4b7d194f41a732367a32c38073cfcabf88
Junnan <yingjunnan1996@gmail.com>
2022-04-11 15:16:24 +0800

    Update replace_zabbix_font.sh

--- replace_zabbix_font.sh
+++ replace_zabbix_font.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y wget
 echo "下载微软雅黑字体"
-wget -O https://raw.githubusercontent.com/chenqing/ng-mini/master/font/msyh.ttf /usr/share/fonts/dejavu/msyh.ttf
+wget -O /usr/share/fonts/dejavu/msyh.ttf https://blogcdn.dadiopi.top/msyh.ttf 
 echo "备份原软链接"
 mv /etc/alternatives/zabbix-web-font /etc/alternatives/zabbix-web-font-bak
 echo "新建软链接"
